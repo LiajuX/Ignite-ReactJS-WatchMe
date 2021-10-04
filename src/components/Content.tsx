5fcc515cb81ec93c5b71a4b2c8ede2e27e41382b
Júlia Brito <julialbritto@gmail.com>
2021-10-04 13:20:12 -0300

    commit

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,24 @@
-export function Content() {
-  // Complete aqui
+import { ReactNode } from 'react';
+
+import '../styles/content.scss';
+
+interface ContentProps {
+  title: string;
+  children: ReactNode;
+}
+
+export function Content({ title, children }: ContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> { title }</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">{ children }</div>
+      </main>
+    </div>
+  );
 }
